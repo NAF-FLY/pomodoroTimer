73e3c8d22ccc8d415c54058ccb87a1bf17f24dff
Vadim Patsyuk <53238795+NAF-FLY@users.noreply.github.com>
2022-09-05 02:38:03 +0300

    Update pomodoro.js

--- pomodoro.js
+++ pomodoro.js
@@ -102,7 +102,7 @@ $(document).ready(function() {
 	$(".switch").click(function() {
 		if ($(this).hasClass("play")) {
 			pomodoro.runTimer();
-			$(this).html('<i class="pause icon"></i> Pause');
+			$(this).html('<i class="pause icon"></i> Пауза');
 			$(".reset").attr("disabled", true);
 		} else if ($(this).hasClass("pause")) {
 			pomodoro.pauseTimer();
